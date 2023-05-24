9de81ca7eeb3cb063a818579dac3918dfc36fbfd
VvsJaemin <djd453216@nate.com>
2023-05-24 21:50:21 +0900

    시큐리티 로그인, 로그아웃, 회원가입 구현

---
+++ src/main/java/com/practice/blog/dto/AddUserRequest.java
@@ -0,0 +1,13 @@
+package com.practice.blog.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/practice/blog/service/UserService.java
@@ -0,0 +1,24 @@
+package com.practice.blog.service;
+
+import com.practice.blog.domain.User;
+import com.practice.blog.dto.AddUserRequest;
+import com.practice.blog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+
+    }
+}
